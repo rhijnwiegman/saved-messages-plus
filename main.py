36761468cf0fa1ca3f8c3abb12f0bd03df3132b1
rhijn <rhijn.wiegman@student.uva.nl>
2024-08-04 17:35:54 +0200

    add buttons in response

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Final
-from telegram import Update
+from telegram import *
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
 TOKEN: Final = '7127271146:AAHV8YM3lEXkqA2UhFIv3JMX-Zt9qLJbSdY'
@@ -14,3 +14,40 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('')
+
+# responses
+
+def handle_response(text: str) -> str:
+    return text
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+    
+    buttons = [[InlineKeyboardButton('Personal', callback_data='personal'), InlineKeyboardButton('Urls', callback_data='urls'), InlineKeyboardButton('Personal', callback_data='personal'),InlineKeyboardButton('Personal', callback_data='personal'), InlineKeyboardButton('Personal', callback_data='personal'), InlineKeyboardButton('Personal', callback_data='personal'), InlineKeyboardButton('Personal', callback_data='personal') ], [InlineKeyboardButton('Study', callback_data='study')]]
+    response: str = handle_response(text)
+
+    # debug
+    print('Bot:', response)
+    await context.bot.send_message(chat_id=update.effective_chat.id, reply_markup=InlineKeyboardMarkup(buttons), text=response)
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused error {context.error}')
+
+if __name__ == '__main__':
+    print('starting bot...')
+    app = Application.builder().token(TOKEN).build()
+
+    # commands
+    app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('help', help_command))
+    app.add_handler(CommandHandler('custom', custom_command))
+
+    # messages
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+    # errors
+    app.add_error_handler(error)
+
+    print('polling...')
+    app.run_polling(poll_interval=3)
